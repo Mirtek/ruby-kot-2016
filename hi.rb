6e18d9c09c2388fa72f157291433381b14af28d8
Denis Toloshny <gg.zyus@gmail.com>
2016-10-15 15:47:33 +0300

    added /message/:id route
    shows unencrypted message from predifined dataset

--- hi.rb
+++ hi.rb
@@ -1,10 +1,28 @@
 require 'sinatra'
+require 'sequel'
+
+DB = Sequel.sqlite
+
+DB.create_table :items do
+	primary_key :id
+	String :text
+	Integer :count
+	Integer :timecreated
+  # String :fancyid
+end
+
+items = DB[:items]
+
+items.insert(:text => 'Hello world', :count => 0, :timecreated => 0)
+items.insert(:text => 'Top kek', :count => 0, :timecreated => 0)
+
 
 get '/hi' do
 	"Hello World!"	
 end
 
 
-get '/' do
-	erb :index
+get '/message/:id' do
+	@message = items.select(:text)[:id => params[:id]][:text]
+	erb :message, :locals => {'message' => @message}
 end
