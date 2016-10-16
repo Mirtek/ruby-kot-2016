51d4df0060858433a3c5360f620f8ee5f621377f
Denis Toloshny <gg.zyus@gmail.com>
2016-10-16 11:07:01 +0300

    added /messagelink/fancy:id and db connection for heroku
    fancy link - secure link to message

--- hi.rb
+++ hi.rb
@@ -1,20 +1,21 @@
 require 'sinatra'
 require 'sequel'
 
-DB = Sequel.sqlite
+# prod DB = Sequel.connect(ENV['DATABASE_URL'])
+DB = Sequel.sqlite # dev
 
 DB.create_table :items do
 	primary_key :id
 	String :text
 	Integer :count
 	Integer :timecreated
-  # String :fancyid
+    String :fancyid
 end
 
 items = DB[:items]
 
-items.insert(:text => 'Hello world', :count => 0, :timecreated => 0)
-items.insert(:text => 'Top kek', :count => 0, :timecreated => 0)
+items.insert(:text => 'Hello world', :count => 0, :timecreated => 0, :fancyid => SecureRandom.urlsafe_base64)
+items.insert(:text => 'Top kek', :count => 0, :timecreated => 0, :fancyid => "rhewkdfh")
 
 
 get '/' do
@@ -22,10 +23,19 @@ get '/' do
 end
 
 post '/' do
-	items.insert(:text => params[:secret_message], :count =>0, :timecreated => 0)
+	fancyid = SecureRandom.urlsafe_base64
+	items.insert(:text => params[:secret_message], :count =>0, :timecreated => 0, :fancyid => fancyid)
+	idlink = request.host_with_port+"/messagelink/"+fancyid
+	erb :message_link, :locals => {'message_link' => idlink}
 end
 
 get '/message/:id' do
 	@message = items.select(:text)[:id => params[:id]][:text]
+	erb :message, :locals => {'message' => @message}
+end
+
+
+get '/messagelink/:fancyid' do
+	@message = items.select(:text)[:fancyid => params[:fancyid]][:text]
 	erb :message, :locals => {'message' => @message}
 end
